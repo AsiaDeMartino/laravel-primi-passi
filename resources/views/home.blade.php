6e14166b299d1876d7f692273225c8e1aa0cc713
asia_demartino@libero.it <asia_demartino@libero.it>
2022-04-11 23:34:17 +0200

    stampati dati dinamici

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,37 @@
+@php 
+
+$utenti = [
+    [
+        "nome"=>"Mario",
+        "cognome"=>"Rossi",
+        "e-mail"=>"MarioRossi@email.com"
+    ],
+    [
+        "nome"=>"Gianni",
+        "cognome"=>"Bianchi",
+        "e-mail"=>"GianniBianchi@email.com"
+    ],
+    [
+        "nome"=>"Anna",
+        "cognome"=>"Verdi",
+        "e-mail"=>"AnnaVerdi@email.com"
+    ],
+    [
+        "nome"=>"Giuseppe",
+        "cognome"=>"Esposito",
+        "e-mail"=>"GiuseppeEsposito@email.com"
+    ],
+    [
+        "nome"=>"Francesco",
+        "cognome"=>"Tani",
+        "e-mail"=>"FrancescoTani@email.com"
+    ]
+
+];
+
+@endphp
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +41,11 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>HELLO WORLD</h1>
+    {{-- <h1>HELLO WORLD</h1> --}}
+    @foreach ($utenti as $item)
+    <h1>Il tuo nome utente è: {{$item['nome']}} {{$item['cognome']}}</h1>
+    <p>L'e-mail che hai utilizzato è: {{$item['e-mail']}}</p>
+    @endforeach
+    
 </body>
 </html>
